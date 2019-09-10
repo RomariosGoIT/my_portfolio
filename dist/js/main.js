48f183b30804fbaaa982b1f7bb013a8bb2778178
RomariosGoIT <romantgt@gmail.com>
2019-09-10 15:31:35 +0300

    update

--- dist/js/main.js
+++ dist/js/main.js
@@ -62,7 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
       this.textEl.innerHTML = `<span class="txt">${this.txt}</span>`;
 
       //Initial type Speed
-      let typeSpeed = 300;
+      let typeSpeed = 200;
 
       this.isDeleted ? (typeSpeed /= 2) : null;
 
